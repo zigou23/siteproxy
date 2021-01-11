c4732a63aa90a95c5dded33b8bab0887a49fe3ef
zigouisim <40587032+zigou23@users.noreply.github.com>
2021-01-11 23:22:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-var password=""
-password=prompt('请输入密码(本网站需输入密码才可以进入):','');
-function custom_close()
-{
-    window.opener=null;
-    window.open('','_self');
-    window.close();        
-}   
-if  (password !='123')
-{
-    custom_close();            
-    //加这一段是为了避免有些浏览器不支持关闭浏览器的js ,所以请求跳转到一个新页面！
-    window.location.href='about:blank';
-}
-
-
-
 var express = require('express')
 var ProxyMiddleware = require('http-proxy-middleware');
 const path = require('path')
